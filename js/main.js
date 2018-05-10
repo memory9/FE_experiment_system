e493f71287e28ecbd29f8364e2f41dd786c760f4
474562318@qq.com <474562318@qq.com>
2018-05-10 14:22:40 +0800

    5.10

--- js/main.js
+++ js/main.js
@@ -1,25 +1,28 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
   /*移动端菜单*/
-  $('.mobile-menu-icon').click(function() {
+  $('.mobile-menu-icon').click(function () {
     $('.templatemo-left-nav').slideToggle();
   });
 
   /*点击 × 时弹出删除对话框*/
-  $('.templatemo-content-widget .fa-times').click(function() {
+  $('.templatemo-content-widget .fa-times').click(function () {
     $(".mask").show(); //显示背景色
     showDialog(); //设置提示对话框的Top与Left
     $(".dialog").show(); //显示提示对话框
   });
 
+  // 设置action属性
   var obj = null;
   var As = document.getElementsByClassName('templatemo-left-nav')[0].getElementsByTagName('a');
-  for (i = 0;i<As.length;i++) {
-    if (window.location.href.indexOf(As[i].href)>=0) {
+  for (i = 0; i < As.length; i++) {
+    if (window.location.href.indexOf(As[i].href) >= 0) {
       obj = As[i];
     }
   }
-  obj.setAttribute('class', 'active');
+  if (obj) {
+    obj.setAttribute('class', 'active');
+  }
 
   function showDialog() {
     var objW = $(window); //当前窗口
@@ -41,31 +44,31 @@ $(document).ready(function() {
     });
   };
 
-  $(window).resize(function() { //页面窗口大小改变事件
+  $(window).resize(function () { //页面窗口大小改变事件
     if (!$(".dialog").is(":visible")) {
       return;
     }
     showDialog(); //设置提示对话框的Top与Left
   });
 
-  $("#Button3").click(function() { //注册取消按钮点击事件
+  $("#Button3").click(function () { //注册取消按钮点击事件
     $(".dialog").hide();
     $(".mask").hide();
   });
 
-  $("#Button2").click(function() { //注册确定按钮点击事件
+  $("#Button2").click(function () { //注册确定按钮点击事件
     $(".dialog").hide();
     $(".mask").hide();
     $('.templatemo-content-widget:visible:first .fa-times').parent().slideUp();
   });
 
-  $('#release-announcement').click(function() {
+  $('#release-announcement').click(function () {
     $(".mask").show(); //显示背景色
     showDialog(); //设置提示对话框的Top与Left
     $(".dialog").show(); //显示提示对话框
   });
 
-  $('#release-btn').click(function() { //注册发布按钮点击事件
+  $('#release-btn').click(function () { //注册发布按钮点击事件
     $(".dialog").hide();
     $(".mask").hide();
     if ($("#announcement-form")) {
@@ -74,19 +77,22 @@ $(document).ready(function() {
     if ($("#addmember-form")) {
       $("#addmember-form").submit();
     }
+    if ($("#addcourse-form")) {
+      $("#addcourse-form").submit();
+    }
   });
 
-  $('.edit-member').click(function() {
+  $('.edit-member').click(function () {
     var bj = $(this).html();
     if (bj == "编辑") {
       var tr = $(this).parent('td').parent('tr');
-      tr.find("td.edit").each(function() {
+      tr.find("td.edit").each(function () {
         var old = $(this).html();　　　　　　
         $(this).html("<input type='text' name='editname' class='text' value=" + old + " >");
       })
       $(this).html("保存");
     } else if (bj == '保存') {　　　　
-      $('input[name=editname]').each(function() {　　　　　　
+      $('input[name=editname]').each(function () {　　　　　　
         // var old = $(this).html();　　　　　　
         var newfont = $(this).parent('td').parent('tr').children().find('input').val();　　　　　　
         $(this).parent('td').html(newfont);　　　　
@@ -95,65 +101,34 @@ $(document).ready(function() {
     }
   });
 
-  $('.select-course').click(function() {
+  $('.select-course').click(function () {
     var xk = $(this).html();
     if (xk == "选课") {
       var tr = $(this).parent('td').parent('tr');
-    $(this).html("已选")
+      $(this).html("已选")
     }
   })
 
-  $('.course-detail').click(function() {
+  $('.course-detail').click(function () {
     $(".mask").show(); //显示背景色
     showDialog(); //设置提示对话框的Top与Left
     $(".dialog").show(); //显示提示对话框
   })
 
-  $('.delete-member').click(function() {
+  $('.delete-member').click(function () {
     var tr = $(this).parent('td').parent('tr');
     tr.remove();
   });
 
-  $('.add-course').click(function() {
-    var last_tr = $('table tbody tr:last');
-    var new_tr = "<tr><td class='edit'></td><td class='edit'></td><td class='edit'></td><td class='edit'></td><td class='edit'></td><td width='200'><button class='templatemo-edit-btn edit-member' type='button'>编辑</button>&nbsp;<button class='templatemo-delete-btn delete-member' type='button'>删除</button></td></tr>"
-    last_tr.after(new_tr);
-    $('.edit-member').click(function() {
-      var bj = $(this).html();
-      if (bj == "编辑") {
-        var tr = $(this).parent('td').parent('tr');
-        tr.find("td.edit").each(function() {
-          var old = $(this).html();　　　　　　
-          $(this).html("<input type='text' name='editname' class='text' value=" + old + " >");
-        })
-        $(this).html("保存");
-      } else if (bj == '保存') {　　　　
-        $('input[name=editname]').each(function() {　　　　　　
-          // var old = $(this).html();　　　　　　
-          var newfont = $(this).parent('td').parent('tr').children().find('input').val();　　　　　　
-          $(this).parent('td').html(newfont);　　　　
-        })　　　　
-        $(this).html('编辑');　　
-      }
-    })
-
-    $('.delete-member').click(function() {
-      var tr = $(this).parent('td').parent('tr');
-      tr.remove();
-    });
-  });
-
-  //添加active属性
-
   //修改密码
-  $("#oldpassword").blur(function() {
+  $("#oldpassword").blur(function () {
     var param = $("#oldpassword").val();
     $.ajax({
       url: "${HttpPath}/user/checkoldpassword",
       data: {
         oldpassword: param
       },
-      success: function(e) {
+      success: function (e) {
         if (e.code == 1) {
           $("#tip1").html("<font color=\"green\" size=\"2\"> 正确</font>");
         } else {
@@ -162,7 +137,7 @@ $(document).ready(function() {
       }
     });
   });
-  $("#password1").blur(function() {
+  $("#password1").blur(function () {
     var num = $("#password1").val().length;
     if (num < 6) {
       $("#tip2").html("<font color=\"red\" size=\"2\">  密码太短，不应少于6个字符</font>");
@@ -172,7 +147,7 @@ $(document).ready(function() {
       $("#tip2").html("<font color=\"green\" size=\"2\"> 正确</font>");
     }
   });
-  $("#password2").blur(function() {
+  $("#password2").blur(function () {
     var tmp = $("#password1").val();
     var num = $("#password2").val().length;
     if ($("#password2").val() != tmp) {
@@ -185,7 +160,7 @@ $(document).ready(function() {
       }
     }
   });
-  $("#btn").click(function() {
+  $("#btn").click(function () {
     var flag = true;
     var old = $("#oldpassword").val();
     var pass = $("#password1").val();
@@ -204,7 +179,7 @@ $(document).ready(function() {
           oldpassword: old,
           password: pass
         },
-        success: function(e) {
+        success: function (e) {
           if (e.code == 1) {
             $("#tip4").show().html("<font color=\"green\" size=\"3\">  修改成功!</font>");
             $("#oldpassword").val("");
